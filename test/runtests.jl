0baeaa217359506af5f3f023fdee8a07770ad25d
Haroun Meghaichi <haroun.meghaichi@gmail.com>
2024-12-05 22:22:34 -0500

    add test compare to LPjl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using LowOrderLegendre
 using Test
+import LegendrePolynomials
 
 @testset "LowOrderLegendre.jl" begin
     # Write your tests here.
+
+    @testset "Comparing to LegendrePolynomials" begin 
+        for x in range(-1,1,17), l=0:15,k=0:15
+            @test  dnPl(x,l,k) ≈ LegendrePolynomials.dnPl(x,l,k)
+        end 
+    end
 end
